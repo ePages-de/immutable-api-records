969be43ec2c476562d2386b7581acd7a2569130c
Christian Hoffmeister <mail@choffmeister.de>
2016-11-07 16:00:12 +0100

    Fix lint error

--- build/extractIdFromSelfLink.js
+++ build/extractIdFromSelfLink.js
@@ -20,6 +20,6 @@
   function extractIdFromSelfLink(item) {
     var selfHref = item.getIn(['_links', 'self', 'href']);
 
-    return selfHref ? selfHref.match(/([0-9a-f]{8}\-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/)[1] : null;
+    return selfHref ? selfHref.match(/([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/)[1] : null;
   }
 });
--- src/extractIdFromSelfLink.js
+++ src/extractIdFromSelfLink.js
@@ -1,5 +1,5 @@
 export default function extractIdFromSelfLink (item) {
   const selfHref = item.getIn(['_links', 'self', 'href'])
 
-  return selfHref ? selfHref.match(/([0-9a-f]{8}\-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/)[1] : null
+  return selfHref ? selfHref.match(/([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/)[1] : null
 }

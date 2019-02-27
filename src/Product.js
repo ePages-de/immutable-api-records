0a076108da11e886821fb369c857f8cb9f76b0a3
Jonathan Wieben <joni.wieben@icloud.com>
2019-02-27 10:18:45 +0100

    Update: Move variationProduct from product to order

--- src/Product.js
+++ src/Product.js
@@ -27,7 +27,6 @@ const ProductRecord = new Record({
   attributes: null,
   variationAttributes: undefined,
   variationAttributeValues: undefined,
-  variationProduct: undefined,
   visible: null,
   maxOrderQuantity: null,
   shippingPeriod: null,
@@ -45,7 +44,6 @@ export default class Product extends ProductRecord {
       .update('salesPrice', (sp) => sp && new Price(sp))
       .update('listPrice', (lp) => lp && new Price(lp))
       .update('manufacturerPrice', (mp) => mp && new Price(mp))
-      .update('variationProduct', (vm) => vm && new Product(vm))
       .update('refPrice', (rp) => rp && new ReferencePrice(rp))
       .update('shippingPeriod', (sp) => sp && new ShippingPeriod(sp))
       .update('shippingDimension', (sd) => sd && new ShippingDimension(sd))
